9b548f8a427c87d1482a0aa5876d2cdf78662b5f
123456 <napnest@gmail.com>
2025-12-27 19:01:44 +0300

    5.6 Знакомство с WebSocket протоколом. Практика работы с WebSocket протоколом

---
+++ websocket_users_client.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import websockets
+
+
+async def client():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        message = "Привет, сервер!"
+        print(f"Отправляю сообщение: {message}")
+        await websocket.send(message)
+
+        for i in range(5):
+            response = await websocket.recv()
+            print(f"{response}")
+
+        print("Все 5 сообщений получены!")
+
+asyncio.run(client())
