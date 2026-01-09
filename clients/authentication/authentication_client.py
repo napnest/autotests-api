a8f7725676cd9f6bf7bea613afad2984a8197336
123456 <napnest@gmail.com>
2026-01-09 10:03:02 +0300

    добавлены api-клиенты создания курса и создания пользователя

--- clients/authentication/authentication_client.py
+++ clients/authentication/authentication_client.py
@@ -1,7 +1,18 @@
-from clients.api_client import APIClient
-from httpx import Response
 from typing import TypedDict
-from clients.public_http_builder import get_public_http_client  # Импортируем builder
+
+from httpx import Response
+
+from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client
+
+
+class Token(TypedDict):  # Добавили структуру с токенами аутентификации
+    """
+    Описание структуры аутентификационных токенов.
+    """
+    tokenType: str
+    accessToken: str
+    refreshToken: str
 
 
 class LoginRequestDict(TypedDict):
@@ -11,11 +22,20 @@ class LoginRequestDict(TypedDict):
     email: str
     password: str
 
+
+class LoginResponseDict(TypedDict):  # Добавили структуру ответа аутентификации
+    """
+    Описание структуры ответа аутентификации.
+    """
+    token: Token
+
+
 class RefreshRequestDict(TypedDict):
     """
     Описание структуры запроса для обновления токена.
     """
-    refreshToken: str  # Название ключа совпадает с API
+    refreshToken: str
+
 
 class AuthenticationClient(APIClient):
     """
@@ -40,7 +60,12 @@ class AuthenticationClient(APIClient):
         """
         return self.post("/api/v1/authentication/refresh", json=request)
 
-# Добавляем builder для AuthenticationClient
+    # Добавили метод login
+    def login(self, request: LoginRequestDict) -> LoginResponseDict:
+        response = self.login_api(request)  # Отправляем запрос на аутентификацию
+        return response.json()  # Извлекаем JSON из ответа
+
+
 def get_authentication_client() -> AuthenticationClient:
     """
     Функция создаёт экземпляр AuthenticationClient с уже настроенным HTTP-клиентом.
@@ -48,3 +73,4 @@ def get_authentication_client() -> AuthenticationClient:
     :return: Готовый к использованию AuthenticationClient.
     """
     return AuthenticationClient(client=get_public_http_client())
+
--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -6,6 +6,18 @@ from clients.api_client import APIClient
 from clients.public_http_builder import get_public_http_client
 
 
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
 class CreateUserRequestDict(TypedDict):
     """
     Описание структуры запроса на создание пользователя.
@@ -15,6 +27,14 @@ class CreateUserRequestDict(TypedDict):
     lastName: str
     firstName: str
     middleName: str
+
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
 
 
 class PublicUsersClient(APIClient):
@@ -31,8 +51,12 @@ class PublicUsersClient(APIClient):
         """
         return self.post("/api/v1/users", json=request)
 
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
-# Добавляем builder для PublicUsersClient
+
 def get_public_users_client() -> PublicUsersClient:
     """
     Функция создаёт экземпляр PublicUsersClient с уже настроенным HTTP-клиентом.
